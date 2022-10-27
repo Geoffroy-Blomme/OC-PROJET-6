104cfc5a2bec169e4060dc945bfe4780f4b91b16
unknown <geoffroy.blomme@gmail.com>
2022-10-27 15:18:24 +0200

    integration page d'accueil realisee

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,9 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        
+        let photographers = await fetch('data/photographers.json');
+        let photographersJson = await photographers.json();
+        return photographersJson;
     }
 
     async function displayData(photographers) {
